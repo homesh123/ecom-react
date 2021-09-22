1d7d1111e104c2b13f2d3199d0eccb859fb6bf57
homesh_nasre <hnasre@rfxcel.com>
2021-09-22 10:55:52 +0530

    Added shop page and collection component

--- aayanarts/src/directory/directory.component.jsx
+++ aayanarts/src/directory/directory.component.jsx
@@ -11,7 +11,7 @@ class directory extends React.Component{
             imageUrl:'https://i.ibb.co/cvpntL1/hats.png',
             id:1,
             size:'small',
-            linkUrl:'hats'
+            linkUrl:'shop'
         },
         {
             title:'hats',
