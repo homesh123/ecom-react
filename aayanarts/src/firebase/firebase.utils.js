e7080754b93a5c194037a276613000d1296868a5
homesh_nasre <hnasre@rfxcel.com>
2021-09-30 15:21:36 +0530

    Added signup component and implemented firestore to create new user in firebase database

--- aayanarts/src/firebase/firebase.utils.js
+++ aayanarts/src/firebase/firebase.utils.js
@@ -3,13 +3,36 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyAHcrVCf8aKNqEqXiBXVxHY2aM23ZMWZuA",
-    authDomain: "aayanarts-4c211.firebaseapp.com",
-    projectId: "aayanarts-4c211",
-    storageBucket: "aayanarts-4c211.appspot.com",
-    messagingSenderId: "1032396258068",
-    appId: "1:1032396258068:web:d954ef49fa65ed8be52001",
-    measurementId: "G-Z0983NDZKP"
+  apiKey: "AIzaSyDedt5j9ba8BcigI5HqVLfDafIVVcjDmCs",
+  authDomain: "aa123-88b5e.firebaseapp.com",
+  projectId: "aa123-88b5e",
+  storageBucket: "aa123-88b5e.appspot.com",
+  messagingSenderId: "54613514521",
+  appId: "1:54613514521:web:8206c6b586a5ddfae94d88"
+};
+
+  export const createUserProfileDocument = async (userAuth,additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+          await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData
+          });
+        } catch(error){
+          console.log('error creating user', error.message)
+        }
+    }  return userRef;
+
   };
 
 firebase.initializeApp(config);
