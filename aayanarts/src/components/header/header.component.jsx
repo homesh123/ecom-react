65c79d520edccc71ef02e95358a155a340918ae8
homesh_nasre <hnasre@rfxcel.com>
2021-09-30 12:54:47 +0530

    Added redux in application

--- aayanarts/src/components/header/header.component.jsx
+++ aayanarts/src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 import {auth} from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 const Header = ({currentUser}) => (
     <div className="header">
