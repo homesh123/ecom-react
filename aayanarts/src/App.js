dc98dc4f46c96bbd132434176e87a17cc7a39f9f
homesh_nasre <hnasre@rfxcel.com>
2021-10-01 18:24:59 +0530

    redirect page to home page after user signin

--- aayanarts/src/App.js
+++ aayanarts/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
@@ -40,7 +40,7 @@ class App extends React.Component{
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpComp} />
+        <Route path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />): (<SignInAndSignUpComp />)} />
       </Switch>
 
   </div>
@@ -48,7 +48,11 @@ class App extends React.Component{
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser:user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
