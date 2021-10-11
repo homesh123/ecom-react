ba664e5ef6a0fd5474a9c735cebd8dcbb96e6475
homesh_nasre <hnasre@rfxcel.com>
2021-10-11 16:09:47 +0530

    Added redux persist lib to use localstorage to temp save store

--- aayanarts/src/redux/store.js
+++ aayanarts/src/redux/store.js
@@ -1,10 +1,14 @@
 import {createStore,applyMiddleware} from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from  './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer,applyMiddleware(...middlewares))
+export const store = createStore(rootReducer,applyMiddleware(...middlewares))
+
+// created persisted version of store , it is used to store in localstorage or sessionstorage
+export const persistor = persistStore(store);
 
 export default store;
